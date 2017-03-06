2e91c5da4553ad9a39b65300ad312301fbcb2682
arsenaltj <645083398@qq.com>
2017-03-06 17:25:28 +0800

    add_3_6_1

--- src/testhread/MultSeller.java
+++ src/testhread/MultSeller.java
@@ -3,8 +3,36 @@ package testhread;
 public class MultSeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+                  
 	}
 
+}
+class Ticket{
+	private int num;
+	private boolean isSold=false;
+	/**
+	 * @return the num
+	 */
+	public int getNum() {
+		return num;
+	}
+	/**
+	 * @param num the num to set
+	 */
+	public void setNum(int num) {
+		this.num = num;
+	}
+	/**
+	 * @return the isSold
+	 */
+	public boolean isSold() {
+		return isSold;
+	}
+	/**
+	 * @param isSold the isSold to set
+	 */
+	public void setSold(boolean isSold) {
+		this.isSold = isSold;
+	}
+	
 }
